2303fdf152702767c0693ed9093590e265dfcc85
Paul Aderoju <paul.adeboye@yahoo.com>
2023-07-18 17:33:56 +0100

    Responsiveness

--- app/page.js
+++ app/page.js
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200'>
        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-          <div className='bg-white rounded-2xl shadow-2xl flex justify-between w-2/3 max-w-4xl'>
+          <div className='bg-white rounded-2xl shadow-2xl lg:flex lg:w-2/3 lg:max-w-4xl'>
             {/* Sign In Section */}
-            <div className='w-3/5 p-5'>
+            <div className='w-full lg:w-3/5 p-5'>
               <div className='text-left font-bold'>
                 <span className='text-green-500'>Company</span>Name
               </div>
@@ -43,12 +43,12 @@ export default function Home() {
                     <a href="#" className="text-xs">Forgot Password?</a>
                   </div>
 
-                   <a href='#' className='border-2 border-green-500 text-green-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-green-500 hover:text-white transition-all'>Sign Up</a>
+                   <a href='#' className='border-2 border-green-500 text-green-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-green-500 hover:text-white transition-all'>Sign In</a>
                 </div>
               </div>
             </div>
             {/* Sign Up Section */}
-            <div className='w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12'>
+            <div className='w-full lg:w-2/5 bg-green-500 text-white rounded-br-2xl rounded-bl-2xl lg:rounded-tr-2xl lg:rounded-br-2xl py-36 px-12'>
               <h2 className='text-3xl font-bold mb-2'>Hello, Friend!</h2>
               <div className='border-2 w-10 border-white inline-block mb-2'></div>
               <p className='mb-3'>Fill up your personal information and start your journey with us today.</p>
